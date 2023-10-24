632019d991a461da660c37b0a2e4b7b28f635c31
CIKADA <adel.garipovazn@mail.ru>
2023-10-25 00:07:48 +0300

    Add extraReducers

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -2,24 +2,30 @@ import style from './Navigation.module.css';
 import {Container} from "../Container/Container.jsx";
 import classNames from "classnames";
 import {useDispatch, useSelector} from "react-redux";
-import {changeCategory} from "../../store/category/categorySlice.js";
+import {categoryRequestAsync, changeCategory} from "../../store/category/categorySlice.js";
+import {useEffect} from "react";
+import {API_URI} from "../../const.js";
 
 export const Navigation = () => {
 
     const {category, activeCategory} = useSelector((state) => state.category)
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        dispatch(categoryRequestAsync());
+    }, []);
+
     return (
         <nav className={style.navigation}>
             <Container className={style.container}>
                 <ul className={style.list}>
                     {category.map((item, idx) =>
-                        <li key={idx} className={style.item}>
+                        <li key={item.title} className={style.item}>
                             <button
                                 className={classNames(style.button,
                                     activeCategory === idx ? style.button_active : ''
                                 )}
-                                style={{backgroundImage: `url(${item.image})`}}
+                                style={{backgroundImage: `url(${API_URI}/${item.image})`}}
                                 onClick={() => {
                                     dispatch(changeCategory({
                                         indexCategory: idx
