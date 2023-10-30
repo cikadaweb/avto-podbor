a72d91cababf34a1288de89dd8749b3061cda8a5
CIKADA <adel.garipovazn@mail.ru>
2023-10-30 23:00:10 +0300

    Add productSlice extraReducers

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
 import categoryReducer from "./category/categorySlice.js";
+import productReducer from "./product/productSlice.js";
 export const store = configureStore({
     reducer: {
-        category: categoryReducer
+        category: categoryReducer,
+        product: productReducer
     }
 });
