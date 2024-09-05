3654e4efaa6f7e4eeb38f06cd0701eb614b00ce7
Adel <adel.garipovazn@mail.ru>
2024-09-05 17:03:45 +0300

    Add Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,32 @@
-import {Header} from "./components/Header/Header.jsx";
+import TheHeader from "./components/Header/Header.jsx";
 import {Navigation} from "./components/Navigation/Navigation.jsx";
 import {Catalog} from "./components/Catalog/Catalog.jsx";
 import {Footer} from "./components/Footer/Footer.jsx";
 import {Provider} from "react-redux";
 import {store} from "./store/index.js";
 import {ModalDelivery} from "./components/ModalDelivery/ModalDelivery.jsx";
+import Container from '@mui/material/Container';
+import {useState} from "react";
 
 export const App = () => {
 
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = (value) => () => {
+        setIsSidebarOpen(value);
+    };
+
     return (
         <Provider store={store}>
-            <Header/>
+            <TheHeader toggleSidebar={toggleSidebar}/>
             <main>
-                <Navigation/>
-                <Catalog/>
+                <Container maxWidth="xl">
+                    <Navigation/>
+                    <Catalog isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
+                </Container>
             </main>
-            <Footer/>
-            <ModalDelivery/>
+            {/*<Footer/>*/}
+            {/*<ModalDelivery/>*/}
         </Provider>
     )
 }
