211bf13eaf6ebe7d742a7b8240ebd3491093fbb0
Adel <adel.garipovazn@mail.ru>
2024-09-05 16:40:32 +0300

    Add material ui styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,30 @@ import {App} from './App.jsx';
 import 'normalize.css';
 import './index.css';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1976d2',
+        },
+        secondary: {
+            main: '#dc004e',
+        },
+    },
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 )
