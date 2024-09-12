3e62e49a5ab92c8758293f978a049d94b2d79a51
Adel <adel.garipovazn@mail.ru>
2024-09-12 23:40:36 +0300

    [WIP] Add Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,37 @@
-import TheHeader from "./components/Header/Header.jsx";
-import {Navigation} from "./components/Navigation/Navigation.jsx";
-import {Catalog} from "./components/Catalog/Catalog.jsx";
-import {Footer} from "./components/Footer/Footer.jsx";
-import {Provider} from "react-redux";
-import {store} from "./store/index.js";
-import {ModalDelivery} from "./components/ModalDelivery/ModalDelivery.jsx";
-import Container from '@mui/material/Container';
 import {useState} from "react";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import {Provider} from "react-redux";
+
+import Container from '@mui/material/Container';
+import {ModelInfoPage} from "./pages/ModelInfoPage/ModelInfoPage.jsx";
+import {MainPage} from "./pages/MainPage/MainPage.jsx";
+import TheHeader from "./components/Header/Header.jsx";
+import {Footer} from "./components/Footer/Footer.jsx";
+import {ModalDelivery} from "./components/ModalDelivery/ModalDelivery.jsx";
+
+import {store} from "./store/index.js";
+
 
 export const App = () => {
-
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-    const toggleSidebar = (value) => () => {
-        setIsSidebarOpen(value);
-    };
-
     return (
         <Provider store={store}>
-            <TheHeader toggleSidebar={toggleSidebar}/>
-            <main>
-                <Container maxWidth="xl">
-                    <Navigation/>
-                    <Catalog isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
-                </Container>
-            </main>
-            {/*<Footer/>*/}
-            {/*<ModalDelivery/>*/}
+            <BrowserRouter>
+                <TheHeader />
+
+                <main>
+                    <Container maxWidth="xl">
+                        <Routes>
+                            <Route path="/" element={<MainPage />}></Route>
+                            <Route path="/model">
+                                <Route path=":id" element={<ModelInfoPage />} />
+                            </Route>
+                            <Route path="*" element={<Navigate to="/" replace />} />
+                        </Routes>
+                    </Container>
+                </main>
+                {/*<Footer/>*/}
+                {/*<ModalDelivery/>*/}
+            </BrowserRouter>
         </Provider>
     )
 }
