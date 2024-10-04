9c246f67b290479498b50ea8f30ae449fd3b9212
Adel <adel.garipovazn@mail.ru>
2024-10-04 17:15:50 +0300

    Refactor store

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,23 +1,22 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import categoryReducer from "./category/categorySlice";
-import productReducer from "./product/productSlice";
+import carsReducer from "./cars/carsSlice";
 import orderReducer, {localStorageMiddleware} from "./order/orderSlice";
 import modalReducer from "./modaDelivery/modalDeliverySlice"
 import formReducer from "./form/formSlice"
 
 const rootReducer = combineReducers({
     category: categoryReducer,
-    product: productReducer,
+    cars: carsReducer,
     order: orderReducer,
     modal: modalReducer,
     form: formReducer
 });
-export const setupStore = configureStore({
+export const store = configureStore({
     reducer: rootReducer,
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(localStorageMiddleware),
 });
 
 export type RootState = ReturnType<typeof rootReducer>
-export type AppStore = ReturnType<typeof setupStore>
-export type AppDispatch = AppStore['dispatch']
+export type AppDispatch = typeof store.dispatch;
