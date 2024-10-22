5053937859f8d079deeb32e86eff6e18e513b76d
Adel <adel.garipovazn@mail.ru>
2024-10-22 11:43:04 +0300

    [WIP] Add condition to asyncThunk

--- frontend/src/store/cars/carsAsyncActions.ts
+++ frontend/src/store/cars/carsAsyncActions.ts
@@ -4,14 +4,24 @@ import axios from "axios";
 
 import {API_URI, POSTFIX} from "../../const";
 import {ICarItem} from "../../types/cars";
+import {ICarsState} from "./carsSlice";
 
-export const fetchCars = createAsyncThunk(
+export const fetchCars = createAsyncThunk<ICarItem[], string, { state: { cars: ICarsState } }> (
     'cars/fetch',
-    async (category: string, thunkAPI) => {
+    async (category, thunkAPI) => {
         try {
             const response = await axios.get<ICarItem[]>(`${API_URI}${POSTFIX}?category=${category}`);
             return response.data
         } catch (err) {
             return thunkAPI.rejectWithValue('Не удалось выполнить запрос!')
+        }
+    },
+    {
+        condition: (category: string, { getState }) => {
+            const { error } = getState().cars;
+
+            if (error) {
+                return false;
+            }
         }
     })
