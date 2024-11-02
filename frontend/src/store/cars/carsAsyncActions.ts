5824ad9ddd471f3c11fc4697b41656ae62ed49c2
Adel <adel.garipovazn@mail.ru>
2024-11-02 11:53:33 +0300

    Add try/catch cars store

--- frontend/src/store/cars/carsAsyncActions.ts
+++ frontend/src/store/cars/carsAsyncActions.ts
@@ -1,21 +1,27 @@
-import {createAsyncThunk} from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 import axios from "axios";
 
-import {API_URI, POSTFIX} from "../../const";
-import {ICarItem} from "../../types/cars";
-import {ICategory} from "../../types/category";
-import {ICarsState} from "./carsSlice";
+import { API_URI, POSTFIX } from "../../const";
+import { ICarItem } from "../../types/cars";
+import { ICategory } from "../../types/category";
+import { ICarsState } from "./carsSlice";
 
-export const fetchCars = createAsyncThunk<ICarItem[], ICategory['title'],   {
-    state: { cars: ICarsState },
-    rejectValue: string,
-}> (
+export const fetchCars = createAsyncThunk<
+    ICarItem[],
+    ICategory['title'],
+    {
+        state: { cars: ICarsState },
+        rejectValue: string,
+    }
+>(
     'cars/fetch',
-    async (category, {rejectWithValue}) => {
-        const response = await axios.get<ICarItem[]>(`${API_URI}${POSTFIX}?category=${category}`);
-        if(!response) {
-            return rejectWithValue('Не удалось выполнить запрос!')
+    async (category, { rejectWithValue }) => {
+        try {
+            const response = await axios.get<ICarItem[]>(`${API_URI}${POSTFIX}?category=${category}`);
+            return response.data;
+        } catch (err) {
+            return rejectWithValue('Не удалось выполнить запрос!');
         }
-        return response.data;
-    })
+    }
+);
--- frontend/src/store/cars/carsSlice.ts
+++ frontend/src/store/cars/carsSlice.ts
@@ -1,4 +1,4 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 import {ICarItem} from "../../types/cars";
 import {fetchCars} from "./carsAsyncActions";
@@ -22,11 +22,11 @@ const carsSlice = createSlice({
             .addCase(fetchCars.pending, state => {
                 state.error = '';
             })
-            .addCase(fetchCars.fulfilled, (state, action) => {
+            .addCase(fetchCars.fulfilled, (state, action: PayloadAction<ICarItem[]>) => {
                 state.error = '';
                 state.cars = action.payload;
             })
-            .addCase(fetchCars.rejected, (state, action) => {
+            .addCase(fetchCars.rejected, (state, action: PayloadAction<string | undefined>) => {
                 state.error = action.payload;
             })
     }
