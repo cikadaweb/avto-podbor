b5352efef6c8a3b4c38f2efc946813da6e8554ae
Adel <adel.garipovazn@mail.ru>
2024-11-08 16:03:01 +0300

    Change commonConfig

--- frontend/src/api/apiConfigs.ts
+++ frontend/src/api/apiConfigs.ts
@@ -2,7 +2,8 @@ export const commonConfig = {
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
-    }
+    },
+    //withCredentials: true,
 };
 
 export const websocketConfig = {
