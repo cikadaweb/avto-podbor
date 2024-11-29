acd46d3288f10e15cfa67e1a99b561dbd11d713e
Adel <adel.garipovazn@mail.ru>
2024-11-29 17:08:29 +0300

    Refactor checkMileageValidity method

--- frontend/src/helpers/checkMilleageValidity/checkMilleageValidity.ts
+++ frontend/src/helpers/checkMilleageValidity/checkMilleageValidity.ts
@@ -1,26 +1,33 @@
-import {MileageStatuses} from "../../enums/MileageStatuses.enums";
+import { MileageStatuses } from '../../enums/MileageStatuses.enums';
 
-export const checkMileageValidity = (year: number, mileage: number): MileageStatuses => {
-    const currentYear = new Date().getFullYear();
-    const carAge = currentYear - year;
+export const checkMileageValidity = (
+  age: number,
+  mileage: number,
+): MileageStatuses => {
+  const averageMileagePerYear = 15000;
+  const expectedMileage = age * averageMileagePerYear;
 
-    // Средний пробег в год, принят в 15,000 км
-    const averageMileagePerYear = 15000;
-    const expectedMileage = carAge * averageMileagePerYear;
+  if (age < 0 || mileage < 0) {
+    throw new Error(
+      'Некорректные данные: возраст или пробег не могут быть отрицательными.',
+    );
+  }
 
-    if (carAge < 0 || mileage < 0) {
-        throw new Error('Некорректные данные: год выпуска или пробег не могут быть отрицательными.');
-    }
+  if (mileage > expectedMileage * 2) {
+    return MileageStatuses.HEAVY_USAGE;
+  }
 
-    if (mileage < expectedMileage * 0.5) {
-        return MileageStatuses.BELOW_AVERAGE; // Пробег существенно ниже ожидаемого
-    } else if (mileage > expectedMileage * 1.5) {
-        return MileageStatuses.SUSPICIOUSLY_HIGH; // Пробег слишком высок
-    } else if (carAge <= 5 && mileage < 100000) {
-        return MileageStatuses.ROLLED_BACK; // Пробег может быть скручен
-    } else if (mileage > averageMileagePerYear * 2) {
-        return MileageStatuses.HEAVY_USAGE; // Частая эксплуатация
-    } else {
-        return MileageStatuses.REALISTIC; // Пробег выглядит правдоподобно
-    }
+  if (mileage > expectedMileage * 1.5) {
+    return MileageStatuses.SUSPICIOUSLY_HIGH;
+  }
+
+  if (mileage < expectedMileage * 0.5) {
+    return MileageStatuses.ROLLED_BACK;
+  }
+
+  if (mileage < expectedMileage * 0.9) {
+    return MileageStatuses.BELOW_AVERAGE;
+  }
+
+  return MileageStatuses.REALISTIC;
 };
