b4bc1fe625ba596d6da1a009adb6aa9d7618d124
Adel <adel.garipovazn@mail.ru>
2024-11-29 17:07:44 +0300

    Add MileageStatuses comments

--- frontend/src/enums/MileageStatuses.enums.ts
+++ frontend/src/enums/MileageStatuses.enums.ts
@@ -1,7 +1,7 @@
 export enum MileageStatuses {
-    REALISTIC = 'REALISTIC',
-    ROLLED_BACK = 'ROLLED_BACK',
-    BELOW_AVERAGE = 'BELOW_AVERAGE',
-    HEAVY_USAGE = 'HEAVY_USAGE',
-    SUSPICIOUSLY_HIGH = 'SUSPICIOUSLY_HIGH',
-};
+  REALISTIC = 'REALISTIC', // Пробег выглядит правдоподобно
+  ROLLED_BACK = 'ROLLED_BACK', // Пробег может быть скручен
+  BELOW_AVERAGE = 'BELOW_AVERAGE', // Пробег ниже среднего, но не критично
+  HEAVY_USAGE = 'HEAVY_USAGE', // Частая эксплуатация
+  SUSPICIOUSLY_HIGH = 'SUSPICIOUSLY_HIGH', // Пробег слишком высок
+}
